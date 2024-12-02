28599beb917804a8ec4d76427c1159d283165f19
Manuela Walczak <kolo44876@gmail.com>
2024-12-02 15:46:49 +0100

    rozwiazanie

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import numpy as np
+import re
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def tokenizuj(tekst):
+    return [slowo.strip() for slowo in re.split(r'[,\.\?!: ]+', tekst.lower()) if slowo]
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+n = int(input())
+dokumenty = [input().strip() for _ in range(n)]
+zapytanie = input().strip()
+k = int(input())
+
+slowa_zapytanie = tokenizuj(zapytanie)
+tokeny = [tokenizuj(dok) for dok in dokumenty]
+
+unikalne_tokeny = []
+for x in tokeny:
+    for token in x:
+        if token not in unikalne_tokeny:
+            unikalne_tokeny.append(token)
+unikalne_tokeny = set(unikalne_tokeny)
+
+
+macierz_incydencji = []
+for token in unikalne_tokeny:
+    wiersz = []
+    for x in tokeny:
+        if token in x:
+            wiersz.append(1)
+        else:
+            wiersz.append(0)
+    macierz_incydencji.append(wiersz)
+
+macierz_incydencji = np.array(macierz_incydencji)
+
+
+U, sigma, VT = np.linalg.svd(macierz_incydencji.T, full_matrices=False)
+
+U_k = U[:, :k]
+Sigma_k = np.diag(sigma[:k])
+VT_k = VT[:k, :]
+
+sprawdz_zapytanie = []
+for token in unikalne_tokeny:
+    if token in slowa_zapytanie:
+        sprawdz_zapytanie.append(1)
+    else:
+        sprawdz_zapytanie.append(0)
+
+sprawdz_zapytanie = np.array(sprawdz_zapytanie)
+
+q_k = sprawdz_zapytanie @ VT_k.T @ np.linalg.inv(Sigma_k)
+
+
+podobienstwa = []
+for wektor_dokumentu in (U_k @ Sigma_k):
+    iloczyn = np.dot(q_k, wektor_dokumentu)
+    norma_q_k = np.linalg.norm(q_k)
+    norma_wektora = np.linalg.norm(wektor_dokumentu)
+    podobienstwo = iloczyn / (norma_q_k * norma_wektora)
+    podobienstwa.append(round(float(podobienstwo), 2))
+
+
+print(podobienstwa)
